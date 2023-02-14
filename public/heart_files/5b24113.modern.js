44c879058fce1fe5facf767145041d66e07e53a0
Pablo-Mdz <pablocigoy@gmail.com>
2023-02-14 16:00:36 +0100

    remove assets

--- public/heart_files/5b24113.modern.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[379],{938:function(e,c,n){"use strict";n.r(c),c.default=n.p+"0ce037259ce11757cdfee5fa8ee6378f.lottie"}}]);
-//# sourceMappingURL=5b24113.modern.js.map
